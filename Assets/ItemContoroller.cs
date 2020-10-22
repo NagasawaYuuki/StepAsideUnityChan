3f0d217f8d5eb45fb62027c93fc713c9459878fa
NagasawaYuuki <69589388+NagasawaYuuki@users.noreply.github.com>
2020-10-22 12:34:59 +0900

    レッスン6　初回コミット

---
+++ Assets/ItemContoroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ItemContoroller : MonoBehaviour
+{
+    public GameObject mainCamObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mainCamObj = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.mainCamObj.transform.position.z>gameObject.transform.position.z)
+        Destroy(gameObject);
+    }
+}
